ef7301cb5d4ed6596ba86d694c65ec2a81345aed
araj00 <aman02437@gmail.com>
2026-05-21 03:25:17 +0530

    Created api gateway to access lambda function with deployment

--- Building-three-tier-app/CDN/main.tf
+++ Building-three-tier-app/CDN/main.tf
@@ -17,6 +17,10 @@ locals {
   var.tags)
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
+
 # Generate a private S3 bucket
 resource "aws_s3_bucket" "my-bucket-s3" {
   bucket = local.content_bucket_name
@@ -150,18 +154,95 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# AWS IAM role for lambda function 
 resource "aws_iam_role" "my-lambda-iam-role" {
   name               = "${local.name_suffix}-lambda-permission"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-
+# Create lambda function with required handler and iam-roles
 resource "aws_lambda_function" "my-lambda-function" {
-  function_name = "${local.name_suffix}-lambda"
+  function_name = "${local.name_suffix}-RetrieveUserData"
   filename = data.archive_file.lambda_function.output_path
   role = aws_iam_role.my-lambda-iam-role.arn
 
   runtime = "nodejs24.x"
   handler = "getUser.handler"
+}
 
+# Create resource api-gateway
+resource "aws_api_gateway_rest_api" "my-lambda-rest-api" {
+  name = "${local.name_suffix}-rest-api-gateway"
+  description = "This is an API for retrieving user details"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Create an api gateway resource
+resource "aws_api_gateway_resource" "my-lambda-rest-api-resource" {
+  parent_id   = aws_api_gateway_rest_api.my-lambda-rest-api.root_resource_id
+  path_part   = "user"
+  rest_api_id = aws_api_gateway_rest_api.my-lambda-rest-api.id
+}
+
+# Create endpoint of api gateway as per its method
+resource "aws_api_gateway_method" "my-lambda-function-api-method" {
+  rest_api_id = aws_api_gateway_rest_api.my-lambda-rest-api.id
+  resource_id = aws_api_gateway_resource.my-lambda-rest-api-resource.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+# Integrate lambda proxy in api method so that all request body can be deserialized in lambda function
+resource "aws_api_gateway_integration" "my-lambda-function-api-method" {
+  resource_id = aws_api_gateway_resource.my-lambda-rest-api-resource.id
+  rest_api_id = aws_api_gateway_rest_api.my-lambda-rest-api.id
+  http_method = aws_api_gateway_method.my-lambda-function-api-method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.my-lambda-function.invoke_arn
+}
+
+# Allow permission to invoke lambda function through api gateway
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my-lambda-function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:${data.aws_partition.current.partition}:execute-api:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.my-lambda-rest-api.id}/*/${aws_api_gateway_method.my-lambda-function-api-method.http_method}${aws_api_gateway_resource.my-lambda-rest-api-resource.path}"
+}
+
+# Create the deployment of api gateway so that we can use its url instead of direction lambda function uri
+resource "aws_api_gateway_deployment" "my-lambda-function-api-method" {
+  rest_api_id = aws_api_gateway_rest_api.my-lambda-rest-api.id
+  description = "Deployment for staging ${var.environment}"
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.my-lambda-rest-api-resource.id,
+      aws_api_gateway_method.my-lambda-function-api-method.id,
+      aws_api_gateway_integration.my-lambda-function-api-method.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create a staging for the api gateway. Chose dev but it can be production , testing for staging
+resource "aws_api_gateway_stage" "my-lambda-function-api-method" {
+  deployment_id = aws_api_gateway_deployment.my-lambda-function-api-method.id
+  rest_api_id   = aws_api_gateway_rest_api.my-lambda-rest-api.id
+  stage_name    = var.environment
 }
