7ab2755032f2255fc2a7c32fe91f68354174d6e9
araj00 <aman02437@gmail.com>
2026-05-25 02:51:55 +0530

    changed the ingression and egression rule of subnet network

--- VPC-Peering-Connection/main.tf
+++ VPC-Peering-Connection/main.tf
@@ -24,49 +24,163 @@ resource "random_integer" "az_index" {
 }
 
 # Initialize a VPC network with a CIDR block
-resource "aws_vpc" "Private-VPC-Network" {
-  count = 2
-  cidr_block       = "10.${count.index}.0.0/16"
+resource "aws_vpc" "Private-VPC-Network-A" {
+  cidr_block       = "10.1.0.0/16"
   instance_tenancy = "default"
 
   tags = merge(local.common_tags, {
-    Name        = "${local.name_suffix}-Priavte-VPC-${count.index}"
+    Name        = "${local.name_suffix}-Priavte-VPC-A"
     Description = "A private VPC network for isolated network"
   })
 }
 
-resource "aws_subnet" "private-subnet-VPC" {
-  count = length(aws_vpc.Private-VPC-Network)
-  vpc_id            = aws_vpc.Private-VPC-Network[count.index].id
-  cidr_block        = cidrsubnet(aws_vpc.Private-VPC-Network[count.index].cidr_block, 8, 2)
+resource "aws_vpc" "Private-VPC-Network-B" {
+  cidr_block       = "10.2.0.0/16"
+  instance_tenancy = "default"
+
+  tags = merge(local.common_tags, {
+    Name        = "${local.name_suffix}-Priavte-VPC-B"
+    Description = "A private VPC network for isolated network"
+  })
+}
+
+resource "aws_subnet" "private-subnet-VPC-A" {
+  vpc_id            = aws_vpc.Private-VPC-Network-A.id
+  cidr_block        = cidrsubnet(aws_vpc.Private-VPC-Network-A.cidr_block, 8, 2)
   availability_zone = data.aws_availability_zones.available.names[random_integer.az_index.result]
 
   tags = merge(local.common_tags, {
-    Name        = "${local.name_suffix}-private-subnet-VPC-${count.index}"
+    Name        = "${local.name_suffix}-private-subnet-VPC-A"
     Description = "A private subnet network for VPC"
   })
 }
 
+resource "aws_subnet" "private-subnet-VPC-B" {
+  vpc_id            = aws_vpc.Private-VPC-Network-B.id
+  cidr_block        = cidrsubnet(aws_vpc.Private-VPC-Network-B.cidr_block, 8, 2)
+  availability_zone = data.aws_availability_zones.available.names[random_integer.az_index.result]
+
+  tags = merge(local.common_tags, {
+    Name        = "${local.name_suffix}-private-subnet-VPC-B"
+    Description = "A private subnet network for VPC"
+  })
+}
 resource "aws_vpc_peering_connection" "VPC-peercon" {
-  peer_vpc_id   = aws_vpc.Private-VPC-Network[0].id
-  vpc_id        = aws_vpc.Private-VPC-Network[1].id
+  peer_vpc_id   = aws_vpc.Private-VPC-Network-A.id
+  vpc_id        = aws_vpc.Private-VPC-Network-B.id
   auto_accept   = true
 }
 
 resource "aws_default_route_table" "VPC-1-route-table" {
-  default_route_table_id = aws_vpc.Private-VPC-Network[0].default_route_table_id
+  default_route_table_id = aws_vpc.Private-VPC-Network-A.default_route_table_id
 
   route {
-    cidr_block = aws_vpc.Private-VPC-Network[1].cidr_block
+    cidr_block = aws_vpc.Private-VPC-Network-B.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.VPC-peercon.id
   }
 }
 
 resource "aws_default_route_table" "VPC-2-route-table" {
-  default_route_table_id = aws_vpc.Private-VPC-Network[1].default_route_table_id
+  default_route_table_id = aws_vpc.Private-VPC-Network-B.default_route_table_id
 
   route {
-    cidr_block = aws_vpc.Private-VPC-Network[0].cidr_block
+    cidr_block = aws_vpc.Private-VPC-Network-A.cidr_block
     vpc_peering_connection_id = aws_vpc_peering_connection.VPC-peercon.id
+  }
+}
+
+resource "aws_default_network_acl" "aws-deny-all-outer-traffic-to-VPC-A" {
+  
+  default_network_acl_id = aws_vpc.Private-VPC-Network-A.default_network_acl_id
+
+  ingress {
+    protocol   = "icmp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-B.cidr_block
+    from_port  = 0
+    to_port    = 0
+    icmp_code = 0
+    icmp_type = 8
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-B.cidr_block
+    from_port  = 22
+    to_port    = 22
+    icmp_code = 0
+    icmp_type = 8
+  }
+  egress {
+    protocol   = "icmp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-B.cidr_block
+    from_port  = 0
+    to_port    = 0
+    icmp_code = 0
+    icmp_type = 0
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-B.cidr_block
+    from_port  = 1024
+    to_port    = 65535
+    icmp_code = 0
+    icmp_type = 0
+  }
+}
+
+resource "aws_default_network_acl" "aws-deny-all-outer-traffic-to-VPC-B" {
+  
+  default_network_acl_id = aws_vpc.Private-VPC-Network-B.default_network_acl_id
+
+  ingress {
+    protocol   = "icmp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-A.cidr_block
+    from_port  = 0
+    to_port    = 0
+    icmp_code = 0
+    icmp_type = 8
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-A.cidr_block
+    from_port  = 22
+    to_port    = 22
+    icmp_code = 0
+    icmp_type = 8
+  }
+  egress {
+    protocol   = "icmp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-A.cidr_block
+    from_port  = 0
+    to_port    = 0
+    icmp_code = 0
+    icmp_type = 0
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = aws_vpc.Private-VPC-Network-A.cidr_block
+    from_port  = 1024
+    to_port    = 65535
+    icmp_code = 0
+    icmp_type = 0
   }
 }
